873504b34d77fa13b211fe7fc66424375fb61f4f
LeonardoBuonanno <rm98315@fiap.com.br>
2023-04-28 20:56:06 -0300

    index e js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// "use strict";
+// console.log("Olá Mundo!!");
+
+// var nr1 = "10";
+// var nr2 = "5";
+
+// Realizando oprações com Strings;
+// Função parseInt...;
+// console.log(parseInt(nr1)+ parseInt(nr2));
+
+nome = "Ale";
+console.log(nome)
+
+if(nome1 != ""){
+    let nome1 = "Buo";
+}
+
+console.log(nome1);
