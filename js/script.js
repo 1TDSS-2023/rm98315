f65f02413c5b70528221d0864f9f60f1a05f5efc
LeoBuonanno <rm98315@fiap.com.br>
2023-05-03 20:52:11 -0300

    defer /addEventListener/getById/Math/Interpolação

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-// "use strict";
+"use strict";
 // console.log("Olá Mundo!!");
 
 // var nr1 = "10";
 // var nr2 = "5";
 
-// Realizando oprações com Strings;
-// Função parseInt...;
-// console.log(parseInt(nr1)+ parseInt(nr2));
+// //Realizando operações com Strings e realizando o parse com a
+// //função parseInt...;
+// console.log(parseInt(nr1)+parseInt(nr2));
 
-nome = "Ale";
-console.log(nome)
+// var nome1 = "Dubas";
+// // let nome2 = "Serbatinho";
+// // const nome3 ="Julia";
+// nome = "Ale";
+// console.log(nome);
 
-if(nome1 != ""){
-    let nome1 = "Buo";
-}
+// if(nome1 != ""){
+//     let nome1 = "Julia";
+// }
 
-console.log(nome1);
+// var nome
+
+// console.log(nome1);
+
+/* <button vallue ="click" id="beu-btn">Click</button> */
+const btn = document.getElementById("meu-btn");
+
+// PRIMEIRO PASSO PARA TRANSFORMAÇÃO PARA O BOTÃO EM RGB...
+btn.addEventListener("click", function(){
+    // console.log(this.textContent);
+    // função matematica math
+    // random = retorna um numero aleatorio entre 0 e 1: exemplo 0.638452345
+    //  floor = arredondao numero para baixo
+    //  ceil = arredonda o numeor para cima
+    //  round = arredonda o numero aleatoriamente
+
+    let r=0, g=0, b=0
+    
+    r = Math. round(Math.random() * 255);
+    g = Math. round(Math.random() * 255);
+    b = Math. round(Math.random() * 255);
+
+    // this.setAttribute("style", "background-color:rgb("+r+", "+g+", "+b+");");
+    this.setAttribute("style", `background-color:rgb(${r}, ${g}, ${b});`);
+ 
+} );
